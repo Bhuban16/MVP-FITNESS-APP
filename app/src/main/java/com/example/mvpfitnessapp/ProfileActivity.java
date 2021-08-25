b0645eaafa383ac3af282866e7c03d3a53eb7d84
gaajendren123@gmail.com <gaajendren123@gmail.com>
2021-08-26 02:52:25 +0800

    backlog 2

---
+++ app/src/main/java/com/example/mvpfitnessapp/ProfileActivity.java
@@ -0,0 +1,143 @@
+package com.example.mvpfitnessapp;
+
+import android.net.Uri;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.MenuItem;
+import android.view.View;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
+import android.util.DisplayMetrics;
+import com.google.android.gms.tasks.OnFailureListener;
+import java.io.IOException;
+
+
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private ImageView profilePic;
+    private TextView profileName, profileEmail,profilePhoneNumber;
+    private Button profileUpdate, changePassword;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseStorage firebaseStorage;
+
+    private static int PICK_IMAGE = 123;
+    Uri imagePath;
+    private StorageReference storageReference;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == PICK_IMAGE && resultCode == RESULT_OK && data.getData() != null){
+            imagePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imagePath);
+                profilePic.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        profilePic = findViewById(R.id.ivProfilePic);
+        profileName = findViewById(R.id.tvProfileName);
+        profileEmail = findViewById(R.id.tvProfileEmail);
+        profilePhoneNumber = findViewById(R.id.tvProfilePhoneNumber);
+        profileUpdate = findViewById(R.id.btnProfileUpdate);
+        changePassword = findViewById(R.id.btnChangePassword);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+
+
+        final DatabaseReference databaseReference = firebaseDatabase.getReference("User Info").child(firebaseAuth.getUid());
+        storageReference = firebaseStorage.getReference();
+
+        StorageReference mImageRef = FirebaseStorage.getInstance().getReference().child(firebaseAuth.getUid()).child("Images").child("Profile Picture");
+        final long ONE_MEGABYTE = 1024 * 1024;
+
+        mImageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                Bitmap bm = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                DisplayMetrics dm = new DisplayMetrics();
+                getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+                profilePic.setMinimumHeight(dm.heightPixels);
+                profilePic.setMinimumWidth(dm.widthPixels);
+                profilePic.setImageBitmap(bm);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                UserProfile userProfile = dataSnapshot.getValue(UserProfile.class);
+                //if (userProfile != null) {
+                    profileName.setText("Name: " + userProfile.getUserName());
+                    profilePhoneNumber.setText("Phone Number: " + userProfile.getUserPhoneNumber());
+                    profileEmail.setText("Email: " + userProfile.getUserEmail());
+                //}
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(ProfileActivity.this, databaseError.getCode(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+     profileUpdate.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(ProfileActivity.this, UpdateProfile.class));
+        }
+    });
+
+        changePassword.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(ProfileActivity.this, UpdatePassword.class));
+        }
+    });
+}
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+}
+}
---
+++ app/src/main/java/com/example/mvpfitnessapp/UpdateProfile.java
@@ -0,0 +1,186 @@
+package com.example.mvpfitnessapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import java.io.IOException;
+import android.graphics.BitmapFactory;
+import android.util.DisplayMetrics;
+
+
+public class UpdateProfile extends AppCompatActivity {
+
+    private EditText newUserName, newUserPhoneNumber, newUserEmail;
+    private Button save;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private ImageView updateProfilePic;
+    private static int PICK_IMAGE = 123;
+    Uri imagePath;
+    private StorageReference storageReference;
+    private FirebaseStorage firebaseStorage;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data.getData() != null) {
+            imagePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imagePath);
+                updateProfilePic.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_profile);
+
+        newUserName = findViewById(R.id.etNameUpdate);
+        newUserPhoneNumber = findViewById(R.id.etPhoneNumberUpdate);
+        newUserEmail = findViewById(R.id.etEmailUpdate);
+        save = findViewById(R.id.btnSave);
+        updateProfilePic = findViewById(R.id.ivProfileUpdate);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+
+        final DatabaseReference databaseReference = firebaseDatabase.getReference("User Info").child(firebaseAuth.getUid());
+        storageReference = firebaseStorage.getReference();
+
+        StorageReference mImageRef = FirebaseStorage.getInstance().getReference().child(firebaseAuth.getUid()).child("Images").child("Profile Picture");
+        final long ONE_MEGABYTE = 1024 * 1024;
+
+        mImageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                Bitmap bm = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                DisplayMetrics dm = new DisplayMetrics();
+                getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+                updateProfilePic.setMinimumHeight(dm.heightPixels);
+                updateProfilePic.setMinimumWidth(dm.widthPixels);
+                updateProfilePic.setImageBitmap(bm);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
+
+        updateProfilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select image"), PICK_IMAGE);
+            }
+        });
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                UserProfile userProfile = dataSnapshot.getValue(UserProfile.class);
+                newUserName.setText(userProfile.getUserName());
+                newUserPhoneNumber.setText(userProfile.getUserPhoneNumber());
+                newUserEmail.setText(userProfile.getUserEmail());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(UpdateProfile.this, databaseError.getCode(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = newUserName.getText().toString();
+                String number = newUserPhoneNumber.getText().toString();
+                String email = newUserEmail.getText().toString();
+
+                UserProfile userProfile = new UserProfile(email, name, number);
+
+                databaseReference.setValue(userProfile);
+
+                StorageReference imageReference = storageReference.child(firebaseAuth.getUid()).child("Images").child("Profile Picture"); //User Id/Images/Profile_pic
+                UploadTask uploadTask = imageReference.putFile(imagePath);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(UpdateProfile.this, "Photo exceeds 1mb", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(UpdateProfile.this, "Upload Successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(UpdateProfile.this, UpdateProfile.class));
+                    }
+            });
+        }
+    });
+        }
+
+    public Boolean validate(){
+        boolean result = false;
+
+        String name = newUserName.getText().toString();
+        String number = newUserPhoneNumber.getText().toString();
+        String email = newUserEmail.getText().toString();
+
+        if(name.isEmpty()) {
+            Toast.makeText(this, "Name can't be empty.", Toast.LENGTH_SHORT).show();
+        }else if(number.isEmpty()){
+            Toast.makeText(this, "Phone number can't be empty.", Toast.LENGTH_SHORT).show();
+        }else if(email.isEmpty()){
+            Toast.makeText(this, "Email can't be empty.", Toast.LENGTH_SHORT).show();
+        }else {
+            result = true;
+        }
+        return result;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    }
