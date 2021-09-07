aeb1c7e029bc18888879267bc57f4e27ff14b11c
rammraja30@gmail.com <rammraja30@gmail.com>
2021-09-08 00:33:59 +0800

    fullscreen

---
+++ app/src/main/java/com/example/mvpfitnessapp/Fullscreen.java
@@ -0,0 +1,14 @@
+package com.example.mvpfitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fullscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+    }
+}
