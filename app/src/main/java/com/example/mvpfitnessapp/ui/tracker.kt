e7dc234b65ba4de589df4c85bf46133b8ce4ba1d
Bhuban <bhubansundram@gmail.com>
2021-09-28 16:22:07 +0800

    resetTracker

--- app/src/main/java/com/example/mvpfitnessapp/ui/tracker.kt
+++ app/src/main/java/com/example/mvpfitnessapp/ui/tracker.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import com.example.mvpfitnessapp.R
 import com.example.mvpfitnessapp.callback.stepsCallback
 import com.example.mvpfitnessapp.helper.GeneralHelper
+import com.example.mvpfitnessapp.helper.PrefsHelper
 import com.example.mvpfitnessapp.service.StepDetectorService
 import kotlinx.android.synthetic.main.activity_tracker.*
 
@@ -20,6 +21,13 @@ class tracker : AppCompatActivity(), stepsCallback {
         startService(intent)
 
         StepDetectorService.subscribe.register(this)
+
+        textReset.setOnClickListener{
+            PrefsHelper.putString("TodayDate" , "")
+            PrefsHelper.putInt("Steps" , 0)
+            PrefsHelper.putInt("FSteps" , 0)
+            subscribeSteps(0)
+        }
     }
 
     override fun subscribeSteps(steps: Int) {
